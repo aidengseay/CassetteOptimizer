10e39f3a506b003258b667975502be8221216b19
aidenseay <ags392@nau.edu>
2023-12-17 20:01:23 -1000

    added fixed getSongList funct

--- CassetteInputUtility.py
+++ CassetteInputUtility.py
@@ -14,26 +14,30 @@ from spotipy.oauth2 import SpotifyOAuth
 
 def getSongList(playlist_url):
 
-    # Get Spotify API credentials from environment variables #TODO: Wrong
-    SPOTIPY_CLIENT_ID = os.environ['SPOTIPY_CLIENT_ID']
-    SPOTIPY_CLIENT_SECRET = os.environ['SPOTIPY_CLIENT_SECRET']
+    # Get Spotify API credentials from environment variables
+    SPOTIPY_CLIENT_ID = os.getenv('SPOTIPY_CLIENT_ID')
+    SPOTIPY_CLIENT_SECRET = os.getenv('SPOTIPY_CLIENT_SECRET')
+    SPOTIPY_REDIRECT_URI = 'http://localhost:8888/callback'
 
     # Create a SpotifyOAuth object
     sp = spotipy.Spotify(auth_manager=SpotifyOAuth(client_id=SPOTIPY_CLIENT_ID,
                                             client_secret=SPOTIPY_CLIENT_SECRET,
+                                            redirect_uri=SPOTIPY_REDIRECT_URI,
                                                  scope='playlist-read-private'))
 
     # Extract playlist ID from the URL
     playlist_id = playlist_url.split('/')[-1]
 
-    # Get the tracks from the playlist
+    # Get the tracks from the playlist (max 100 songs)
     results = sp.playlist_tracks(playlist_id)
 
     # Print information about each track
     for track in results['items']:
         track_name = track['track']['name']
         artist_name = track['track']['artists'][0]['name']
-        print(f"Track: {track_name} - Artist: {artist_name}")
+        duration_s = track['track']['duration_ms'] // 1000
+
+        print(f"Track: {track_name} - Artist: {artist_name} - Duration: {duration_s}")
 
 
 ################################################################################
